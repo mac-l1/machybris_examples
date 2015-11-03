eb1abe37b9d9e6a3ba4de4dbffdce623864637db
mac-l1 <mac.level1@gmail.com>
2015-11-03 06:03:31 +0000

    update make_all.sh to make all new stuff

--- make_all.sh
+++ make_all.sh
@@ -1,14 +1,59 @@
 #!/bin/bash
 set -x # verbose
-SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-# init bin dir for output
+SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $SCRIPT_DIR
-mkdir -p bin
 
 cd test_vpu
+./make.sh $@
+cd $SCRIPT_DIR
+
+cd test_hwcomposer
+./make.sh $@
+cd $SCRIPT_DIR
+
+cd simple_framework
+./make.sh $@
+cd $SCRIPT_DIR
+
+cd arm-examples
+./make.sh $@
+cd $SCRIPT_DIR
+
+if [ $(echo $@|grep -w "clean"|wc -l) != "0" ]; then
+    rm -rf Mindroid.cpp 
+else
+if ! [ -e Mindroid.cpp ]; then
+  git clone https://github.com/esrlabs/Mindroid.cpp.git
+  cd Mindroid.cpp
+  sed 's/-D__ARMv6_CPU_ARCH__ -fPIC -O2/-fPIC -O2 -Wa,-mimplicit-it=thumb/g' Makefile.RPi > Makefile.ARM
+  make -f Makefile.ARM
+  sudo cp libmindroid.so /usr/local/lib
+fi
+fi
+cd $SCRIPT_DIR
+
+if ! [ -x /usr/bin/avconv ]; then
+  sudo apt-get install libav-tools
+fi
+
+cd ffmpeg
+./make.sh $@
+cd ..
+
+cd recorder
+./make.sh $@
+cd $SCRIPT_DIR
+
+cd player
+./make.sh $@
+cd $SCRIPT_DIR
+
+echo "now downloading some movies"
+echo "press ctrl-c to abort"
+
+cd media
 ./make.sh
-cp -a test_vpu $SCRIPT_DIR/bin
 cd $SCRIPT_DIR
 
 exit
