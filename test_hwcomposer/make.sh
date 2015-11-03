199a61e043a5bd621c9e2dda9dd13fc133ca6aba
mac-l1 <mac.level1@gmail.com>
2015-11-03 05:43:51 +0000

    Add test_hwcomposer

---
+++ test_hwcomposer/make.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -x
+
+if [ $(echo $@|grep -w "clean"|wc -l) != "0" ]; then
+  rm -rf test_hwcomposer
+  exit
+fi
+
+g++ -I /usr/local/include/android/ -I /usr/local/include/hybris/hwcomposerwindow/ -I /usr/local/include/hybris/eglplatformcommon/ -o test_hwcomposer test_hwcomposer.cpp -l hybris-common -l hybris-hwcomposerwindow -l hybris-eglplatformcommon -l EGL -l sync -l hardware -l GLESv2
--- test_vpu/make.sh
+++ test_vpu/make.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+set -x
+
+if [ $(echo $@|grep -w "clean"|wc -l) != "0" ]; then
+  rm -rf test_vpu
+  exit
+fi
+
 gcc -o test_vpu test_vpu.c -lvpu
