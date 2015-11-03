1251d3a01e2406417af4a8cbb06d98f56c02e515
mac-l1 <mac.level1@gmail.com>
2015-11-03 05:49:50 +0000

    add ffmpeg static libs from kodi-15.2 supporting hevc demuxing

---
+++ ffmpeg/make.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -x
+
+if [ $(echo $@|grep -w "clean"|wc -l) != "0" ]; then
+  rm -rf ffmpeg-2.6.4-Isengard* ffmpeg-install 
+  exit
+fi
+
+CFLAGS="-mfloat-abi=hard -mfpu=neon -ftree-vectorize -mvectorize-with-neon-quad -ffast-math -O3 -pipe -fstack-protector" ./autobuild.sh -r --disable-optimizations
+
