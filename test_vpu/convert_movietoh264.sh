1988acaba6d654860fec5fc77937c09b5e7392f1
mac-l1 <mac-l1@users.noreply.github.com>
2016-09-24 23:16:31 +0200

    Update convert_movietoh264.sh
    avoid using avconv as h264 stream is not always handled correctly by vpu

--- test_vpu/convert_movietoh264.sh
+++ test_vpu/convert_movietoh264.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-#gst-launch-1.0 filesrc location=$1 ! qtdemux  ! h264parse ! video/x-h264, stream-format=byte-stream, alignment=nal, parsed=true ! filesink location=$1.h264
-avconv -i $1 -c:v copy -bsf:v h264_mp4toannexb -an $1.h264
+# sudo apt-get install gstreamer1.0
+gst-launch-1.0 filesrc location=$1 ! qtdemux  ! h264parse ! video/x-h264, stream-format=byte-stream, alignment=nal, parsed=true ! filesink location=$1.h264
+# avconv -i $1 -c:v copy -bsf:v h264_mp4toannexb -an $1.h264
